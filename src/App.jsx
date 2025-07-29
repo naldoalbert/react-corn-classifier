a7b4451cfee936be1f0ec8f10858edfd3ba6ee50
naldoalbert <albertjgulo@gmail>
2025-07-29 09:58:34 +0700

    About1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from './components/Nav';
-import About from './components/about';
+import About from './components/About';
 import Scan from './components/Scan';
 import Home from './components/Home';
 import Footer from './components/Footer';
